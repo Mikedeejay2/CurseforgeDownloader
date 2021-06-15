00376796d445af797a0bfe7350e7f34360a4f172
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-06-14 22:30:45 -0400

    Add comments

--- curseforge_dl.py
+++ curseforge_dl.py
@@ -6,6 +6,7 @@ import os
 from pathlib import Path
 from datetime import datetime
 
+# Headers used to avoid 403 forbidden when connecting to urls, adds information to the request
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -14,24 +15,38 @@ HEADERS = {
     'Accept-Language': 'en-US,en;q=0.8',
     'Connection': 'keep-alive'}
 
+# Constants for Curseforge and APIs in case of change
 CURSEFORGE = 'curseforge.com'
 CURSEFORGE_FILES = 'https://media.forgecdn.net/files/%s/%s/%s'
 CURSEFORGE_API = 'https://api.cfwidget.com/%s'
 
 
 def __get_datetime(unparsed_time=''):
+    """
+    Gets the datetime of an unparsed time for easy comparison
+
+    :param unparsed_time: The unparsed time retrieved from an API
+    :return: The parsed datetime
+    """
     if '.' not in unparsed_time:
         return datetime.strptime(unparsed_time, '%Y-%m-%dT%H:%M:%SZ')
 
     return datetime.strptime(unparsed_time, '%Y-%m-%dT%H:%M:%S.%fZ')
 
 
-def __get_latest_json(files_section, versions):
+def __get_latest_json(files_json, versions=None):
+    """
+    Gets the latest json version for a Curseforge file
+
+    :param files_json: The files json section of the project
+    :param versions: A list of the versions to be considered
+    :return: The json section for the latest uploaded file to the project based on the versions provided
+    """
     index = 0
     previous_time = datetime.min
     for version in versions:
-        for i, j in enumerate(files_section):
-            if version not in j['versions']:
+        for i, j in enumerate(files_json):
+            if version is not None or version not in j['versions']:
                 continue
             upload_time: datetime
             unparsed_time = j['uploaded_at']
@@ -40,10 +55,17 @@ def __get_latest_json(files_section, versions):
                 previous_time = upload_time
                 index = i
 
-    return files_section[index]
+    return files_json[index]
 
 
 def __get_file_url(url='', file_name=''):
+    """
+    Get the direct download url of a Curseforge file url
+
+    :param url: The original Curseforge project file URL (Not direct download)
+    :param file_name: The name of the file that is being downloaded
+    :return: The direct download to the file
+    """
     id_part_1 = url[len(url) - 7:len(url) - 3]
     id_part_2 = url[len(url) - 3:len(url)]
     while id_part_2.startswith('0'):
@@ -53,6 +75,13 @@ def __get_file_url(url='', file_name=''):
 
 
 def __dl_from_file_url(output_folder='', url='', file_name=''):
+    """
+    Download a file from from a direct download url
+
+    :param output_folder: The output location where the file should be downloaded to
+    :param url: The URL of the file to be downloaded. This should be a direct link.
+    :param file_name: The name of the file that is being downloaded
+    """
     print("Downloading %s" % file_name)
     file_request = urllib.request.Request(__get_file_url(url, file_name), headers=HEADERS)
     file_url = urllib.request.urlopen(file_request)
@@ -70,6 +99,14 @@ def __dl_from_file_url(output_folder='', url='', file_name=''):
 
 
 def download_single(url='', output_folder='', versions=None):
+    """
+    Download the latest version of a Curseforge project from a link
+
+    :param url: The URL of the project page
+    :param output_folder: The output folder location. Where all files will be downloaded to.
+    :param versions: The list of versions that should be considered when downloading
+    (Leaving None will download the latest version regardless of the version)
+    """
     url = url.strip()
     print('Initializing download for url ' + url)
     line_new = url.split(CURSEFORGE + '/')[1]
@@ -84,6 +121,14 @@ def download_single(url='', output_folder='', versions=None):
 
 
 def download_all(path='', output_folder='', versions=None):
+    """
+    Download the latest version of all Curseforge project urls from a text document or similar
+
+    :param path: The path to the input mods list (Text file or similar)
+    :param output_folder: The output folder location. Where all files will be downloaded to.
+    :param versions: The list of versions that should be considered when downloading
+    (Leaving None will download the latest version regardless of the version)
+    """
     mods_file = open(path, 'r')
 
     mods_file_lines = mods_file.readlines()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import curseforge_dl
 
+# Change these values here:
+# The path to the input mods list (Text file or similar)
 MODS_FILE = 'test\\mods.txt'
+# The output folder location. Where all files will be downloaded to.
 OUTPUT_FOLDER = 'test\\output'
+# The list of versions that should be considered when downloading
 VERSIONS = ['1.12.2', '1.12.1', '1.12']
 
 if __name__ == '__main__':
