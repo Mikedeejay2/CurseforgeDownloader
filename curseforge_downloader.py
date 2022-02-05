5ee39b3e085b791511fcc9ca940e0932d7d801f8
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2022-02-04 20:30:57 -0500

    Ensure that ID is retrieved If all else fails, just ask the user for the mod ID

--- curseforge_downloader.py
+++ curseforge_downloader.py
@@ -22,6 +22,7 @@ HEADERS = {
 CURSEFORGE = 'curseforge.com'
 CURSEFORGE_FILES = 'https://media.forgecdn.net/files/%s/%s/%s'
 CURSEFORGE_API = 'https://addons-ecs.forgesvc.net/api/v2/%s'
+CFWIDGET_API = 'https://api.cfwidget.com/%s'
 
 
 class LogLevel(Enum):
@@ -105,20 +106,21 @@ class CurseforgeDownloader:
     # QUERY FUNCTIONS
     #########################################################
 
-    def __query_api(self, args: str, params=None) -> json:
+    def __query(self, api: str, args: str, params=None) -> json:
         if params is None:
             params = {}
-        api_line = 'Unknown'
-        try:
-            api_line = CURSEFORGE_API % args
-            api_request = requests.get(api_line, params=params, headers=HEADERS)
-            if api_request.status_code == 200:
-                api_json = api_request.json()
-                return api_json
-            self.__log_severe("Unable to parse json for API request: %s" % api_line)
-        except urllib.error.HTTPError as e:
-            self.__log_severe('API threw error %s: %s' % (e.code, api_line))
-            return None
+        api_line = api % args
+        api_request = requests.get(api_line, params=params, headers=HEADERS)
+        if api_request.status_code == 200:
+            api_json = api_request.json()
+            return api_json
+        self.__log_severe("Unable to parse json for API request: %s" % api_line)
+
+    def __query_api(self, args: str, params=None) -> json:
+        return self.__query(CURSEFORGE_API, args, params)
+
+    def __query_cfwidget(self, args: str, params=None) -> json:
+        return self.__query(CFWIDGET_API, args, params)
 
     def __retrieve_json_section(self, json_list: json, search: Dict[str, str]):
         # if section != '' and section not in json_parent:
@@ -148,7 +150,8 @@ class CurseforgeDownloader:
             return None
         return section
 
-    def __query_game_id(self, game_slug: str) -> int:
+    def __query_game_id(self, info: Dict[str, Any]) -> int:
+        game_slug = info['game_slug']
         if game_slug in self.cache_games:
             return self.cache_games[game_slug]
         game_json = self.__query_game(game_slug)
@@ -166,11 +169,13 @@ class CurseforgeDownloader:
             'gameId': game_id
         })
         if section is None:
-            self.__log_warning('No game found for: %s' % category_slug)
+            self.__log_warning('No category found for: %s' % category_slug)
             return None
         return section
 
-    def __query_category_id(self, category_slug: str, game_id: int) -> int:
+    def __query_category_id(self, info: Dict[str, Any]) -> int:
+        category_slug = info['category_slug']
+        game_id = info['game_id']
         if category_slug in self.cache_categories:
             return self.cache_categories[category_slug]
 
@@ -182,32 +187,93 @@ class CurseforgeDownloader:
         self.cache_categories[category_slug] = category_id
         return category_id
 
-    def __query_mod_recur(self, game_id: int, section_id: int, mod_slug: str, search: str):
+    def __query_mod_search(self, info: Dict[str, Any], search: str):
+        game_id = info['game_id']
+        category_id = info['category_id']
+        mod_slug = info['mod_slug']
         # mod_json = self.__query_api('mods/search?gameId=%s&categoryId=%s' % (game_id, category_id))
         mod_json = self.__query_api('addon/search', {
             'gameId': game_id,
-            'sectionId': section_id,
+            'sectionId': category_id,
             'searchFilter': search,
             # 'pageSize': 10
         })
         for sub_json in mod_json:
             if 'slug' in sub_json and sub_json['slug'] == mod_slug:
                 return sub_json
-        print(search)
         if len(search) > 2:
             new_search = search
             if '-' in new_search:
                 new_search = new_search[0:new_search.rindex('-')]
             else:
                 new_search = new_search[0:len(new_search) - 1]
-            return self.__query_mod_recur(game_id, section_id, mod_slug, new_search)
-        self.__log_severe("Unable to find mod of name slug: %s     Consider providing project ID instead" % mod_slug)
+            return self.__query_mod_search(info, new_search)
         return None
 
-    def __query_mod(self, game_id: int, section_id: int, mod_slug: str) -> json:
-        return self.__query_mod_recur(game_id, section_id, mod_slug, mod_slug)
+    def __query_mod_cfwidget(self, info: Dict[str, Any]) -> json:
+        result = self.__query_cfwidget('%s/%s/%s' % (info['game_slug'], info['category_slug'], info['mod_slug']))
+        if result is None:
+            self.__log_warning('No mod found for: %s' % info['mod_slug'])
+            return None
+        return result
 
-    def __get_mod_id(self, mod_json: json, mod_slug: str) -> int:
+    def __query_mod_info(self, mod_id: int) -> json:
+        result = self.__query_api('addon/%s' % str(mod_id))
+        if result is None:
+            self.__log_severe('Unable to retrieve mod of ID from API: %s' % mod_id)
+            return None
+        return result
+
+    def __query_mod_manual(self, info: Dict[str, Any]) -> json:
+        mod_json = None
+        while True:
+            mod_id = input("Mod ID: ")
+            if mod_id.lower() == 'exit':
+                self.__log_severe('Skipping mod due to denial of manual user input: %s' % info['mod_slug'])
+                break
+            if not mod_id.isdigit():
+                print('The value entered is not a valid ID. Please try again.')
+                continue
+            mod_json = self.__query_mod_info(int(mod_id))
+            if mod_json is None or 'slug' not in mod_json:
+                print('The value entered is not a valid ID. Please try again.')
+                continue
+            if mod_json['slug'] != info['mod_slug']:
+                print('The required mod \"%s\" does not match the retrieved mod \"%s\". Please enter the correct ID.' %
+                      (info['slug'], mod_json['slug']))
+                continue
+            break
+        return mod_json
+
+    def __query_mod(self, info: Dict[str, Any]) -> json:
+        result = self.__query_mod_search(info, info['mod_slug'])
+        if result is not None:
+            self.__log_info("Mod information retrieved via ForgeSVC API: %s" % info['mod_slug'])
+        if result is None:
+            self.__log_warning('Unable to retrieve mod from ForgeSVC API, trying CFWidget API: %s' % info['mod_slug'])
+            result = self.__query_mod_cfwidget(info)
+            if result is not None:
+                self.__log_info("Mod information retrieved via CFWidget API: %s" % info['mod_slug'])
+        if result is None:
+            print('Unable to get mod \"%s\" through URL provided, please paste the ID of the mod from the mod URL: %s' %
+                  (info['mod_slug'], info['url']))
+            result = self.__query_mod_manual(info)
+            if result is not None:
+                self.__log_info("Mod information retrieved via manual user input: %s" % info['mod_slug'])
+
+        if 'id' not in result:
+            self.__log_severe('Unable to retrieve mod ID; attempted all available methods: %s' % info['mod_slug'])
+            return None
+
+        mod_json = self.__query_mod_info(result['id'])
+
+        if mod_json is None:
+            return None
+        return mod_json
+
+    def __get_mod_id(self, info: Dict[str, Any]) -> int:
+        mod_json = info['mod_json']
+        mod_slug = info['mod_slug']
         if mod_json is None:
             return -1
         if 'id' not in mod_json:
@@ -233,7 +299,7 @@ class CurseforgeDownloader:
     # INTERMEDIARY FUNCTIONS
     #########################################################
 
-    def __get_mod_info(self, url: str) -> Dict[str, Any]:
+    def __get_mod_preinfo(self, url: str) -> Dict[str, Any]:
         url = self.__trim_url(url)
         if not self.__validate_url(url):
             return {}
@@ -242,32 +308,41 @@ class CurseforgeDownloader:
         mod_slug = self.__get_mod_slug(url)
         if len(game_slug) == 0 or len(category_slug) == 0:
             return {}
-
-        game_id = self.__query_game_id(game_slug)
-        if game_id < 0:
-            return {}
-        category_id = self.__query_category_id(category_slug, game_id)
-        if category_id < 0:
-            return {}
-        mod_json = self.__query_mod(game_id, category_id, mod_slug)
-        if mod_json is None:
-            return {}
-        mod_id = self.__get_mod_id(mod_json, mod_slug)
-        if mod_id < 0:
-            return {}
-
         return {
+            'url': url,
             'game_slug': game_slug,
             'category_slug': category_slug,
             'mod_slug': mod_slug,
-            'game_id': game_id,
-            'category_id': category_id,
-            'mod_json': mod_json,
-            'mod_id': mod_id,
         }
+
+    def __get_mod_info(self, url: str) -> Dict[str, Any]:
+        info = self.__get_mod_preinfo(url)
+
+        game_id = self.__query_game_id(info)
+        if game_id < 0:
+            return {}
+        info.update({'game_id': game_id})
+
+        category_id = self.__query_category_id(info)
+        if category_id < 0:
+            return {}
+        info.update({'category_id': category_id})
+
+        mod_json = self.__query_mod(info)
+        if mod_json is None:
+            return {}
+        info.update({'mod_json': mod_json})
+
+        mod_id = self.__get_mod_id(info)
+        if mod_id < 0:
+            return {}
+        info.update({'mod_id': mod_id})
+
+        return info
 
     def __download_single(self, url: str):
         info = self.__get_mod_info(url)
+        print(info)
         if len(info) == 0:
             return
 
@@ -278,3 +353,4 @@ class CurseforgeDownloader:
     def download_all(self):
         for mod_url in self.__read_mods():
             self.__download_single(mod_url)
+            break
