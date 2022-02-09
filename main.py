fd0945efb3693e93dfcf72d8f90adabe3773eaca
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2022-02-08 21:49:42 -0500

    Use versions 1.12 for testing

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ MODS_FILE = 'run\\mods.txt'
 # The output folder location. Where all files will be downloaded to.
 OUTPUT_FOLDER = 'run\\output'
 # The list of versions that should be considered when downloading
-VERSIONS = ['1.16', '1.16.1', '1.16.2', '1.16.3', '1.16.4', '1.16.5']
+VERSIONS = ['1.12', '1.12.1', '1.12.2']
 # The list of versions that should be excluded when downloading
 EXCLUDED = ['Fabric']
 
